5e205c802895ecc2ff02e6a4a98a1d7013f2db37
梁斌 <waterstonelb@gmail.com>
2020-02-17 01:45:08 +0800

    12

--- src/main/java/com/example/demo/controller/TestContorller.java
+++ src/main/java/com/example/demo/controller/TestContorller.java
@@ -1,5 +1,8 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 public class TestContorller {
 }
