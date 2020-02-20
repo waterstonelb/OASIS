d134cbd0e8817f7bd5fde6e86f0772b0bd74ba42
梁斌 <waterstonelb@gmail.com>
2020-02-20 23:26:03 +0800

    解决CORS

---
+++ src/main/java/com/example/demo/config/CORSConfig.java
@@ -0,0 +1,17 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class CORSConfig extends WebMvcConfigurationSupport {
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedMethods("*")
+                .allowedOrigins("*")
+                .exposedHeaders("*");
+        super.addCorsMappings(registry);
+    }
+}
