5080c4c6909621d7bd9e50603d06624859ac7ecc
梁斌 <waterstonelb@gmail.com>
2020-03-02 19:53:09 +0800

    Mock测试标准

--- src/main/java/com/example/demo/vo/ComSearchInpVO.java
+++ src/main/java/com/example/demo/vo/ComSearchInpVO.java
@@ -11,4 +11,14 @@ public class ComSearchInpVO {
     String institution;
     String conference;
     String keyword;
+
+    public ComSearchInpVO(String authors, String institution, String conference, String keyword) {
+        this.authors = authors;
+        this.institution = institution;
+        this.conference = conference;
+        this.keyword = keyword;
+    }
+
+    public ComSearchInpVO() {
+    }
 }
