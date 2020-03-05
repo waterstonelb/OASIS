9004a8b572387ffc1280111ac8225781602a381b
lqk <171850587@smail.nju.edu.cn>
2020-03-05 13:59:53 +0800

    search

--- src/main/java/com/example/demo/service/SerachServiceImpl.java
+++ src/main/java/com/example/demo/service/SerachServiceImpl.java
@@ -2,13 +2,11 @@ package com.example.demo.service;
 
 
 import com.example.demo.dao.*;
-import com.example.demo.dataSource.Data;
 import com.example.demo.po.*;
 import com.example.demo.vo.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
