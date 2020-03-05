bf750e491bdc9d11e445d6b6352bee66efc1be19
lqk <171850587@smail.nju.edu.cn>
2020-03-05 18:10:06 +0800

    author msg add

--- src/main/java/com/example/demo/vo/DocumentVO.java
+++ src/main/java/com/example/demo/vo/DocumentVO.java
@@ -3,11 +3,13 @@ package com.example.demo.vo;
 import com.example.demo.po.Author;
 import com.example.demo.po.Document;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@AllArgsConstructor
 public class DocumentVO {
 
     @ApiModelProperty("文章信息")
