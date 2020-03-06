fcb977fe029f621bdcf764e3002a1805aee229d9
梁斌 <waterstonelb@gmail.com>
2020-03-06 22:49:50 +0800

    更新Paper详情

--- src/main/java/com/example/demo/controller/PaperInfoController.java
+++ src/main/java/com/example/demo/controller/PaperInfoController.java
@@ -6,13 +6,11 @@ import com.example.demo.vo.ResponseVO;
 import io.swagger.annotations.ApiModelProperty;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/paper")
 public class PaperInfoController {
