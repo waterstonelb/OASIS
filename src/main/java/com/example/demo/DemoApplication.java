152624749b83dcbab2208db27488c4d1ea712ff3
lqk <171850587@smail.nju.edu.cn>
2020-04-01 16:03:13 +0800

    cache

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @EnableJpaRepositories
+@EnableCaching
 @SpringBootApplication
 public class DemoApplication {
 
