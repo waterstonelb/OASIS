7e9d0a271e66a20011cc4700bc92f15452f20251
lqk <171850587@smail.nju.edu.cn>
2020-05-25 15:03:30 +0800

    figure service updata

--- src/test/java/com/example/demo/service/FigureServiceImplTest.java
+++ src/test/java/com/example/demo/service/FigureServiceImplTest.java
@@ -43,12 +43,12 @@ public class FigureServiceImplTest {
         assertEquals( idLink1.hashCode(),idLink2.hashCode());
     }
 
-    @Test
-    public void getFieldFigureTest(){
-        ResponseVO<FieldFigureVO> res = figureService.getFieldFigure();
-        System.out.println(res.getData().getLinks().size());
-        System.out.println(res.getData().getNodes().size());
-        assertFalse(res.getData().getLinks().isEmpty());
-        assertFalse(res.getData().getNodes().isEmpty());
-    }
+//    @Test
+//    public void getFieldFigureTest(){
+//        ResponseVO<FieldFigureVO> res = figureService.getFieldFigure();
+//        System.out.println(res.getData().getLinks().size());
+//        System.out.println(res.getData().getNodes().size());
+//        assertFalse(res.getData().getLinks().isEmpty());
+//        assertFalse(res.getData().getNodes().isEmpty());
+//    }
 }
