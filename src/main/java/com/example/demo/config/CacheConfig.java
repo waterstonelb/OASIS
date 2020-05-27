ddfa53464b93eeb64f45d3fc1143ba3acdeed411
lqk <171850587@smail.nju.edu.cn>
2020-05-27 21:54:45 +0800

    disable cache in tests

--- src/main/java/com/example/demo/config/CacheConfig.java
+++ src/main/java/com/example/demo/config/CacheConfig.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConfiguration;
@@ -23,6 +24,7 @@ import java.util.Map;
 @Configuration
 @AutoConfigureAfter(RedisAutoConfiguration.class)
 @EnableCaching
+@Profile("!test")
 public class CacheConfig {
 
     @Value("${redis.hostname:ismzl.com}")
